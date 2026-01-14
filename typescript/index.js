783a42e0da0ee641fe9474396ba76b598a9c8ebb
souhaib benbouzid <souhaib.benbouzid@gmail.com>
2026-01-14 17:45:13 +0100

    playground for python / typescript

---
+++ typescript/index.js
@@ -0,0 +1,5 @@
+function main() {
+    console.log("typescript is working!");
+    return;
+}
+main();
---
+++ typescript/index.ts
@@ -0,0 +1,7 @@
+
+function main() {
+    console.log("typescript is working!");
+    return;
+}
+
+main();
